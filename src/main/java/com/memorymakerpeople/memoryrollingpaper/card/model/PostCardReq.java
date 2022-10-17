7a23d431af4cc8fa577091508e6c7646746a40ce
mntdev <mntdev@mbc.co.kr>
2022-10-17 14:17:34 +0900

    #10 Feat: 카드 생성 Request Dto 추가

---
+++ src/main/java/com/memorymakerpeople/memoryrollingpaper/card/model/PostCardReq.java
@@ -0,0 +1,29 @@
+package com.memorymakerpeople.memoryrollingpaper.card.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class PostCardReq {
+    private String cardText;
+    private String cardColor;
+    private Long paperId;
+    private String fontStyle;
+    private String fontColor;
+    private String cardWriter;
+
+    public Card toEntity() {
+        return Card.builder().
+                cardText(this.cardText).
+                cardColor(this.cardColor).
+                paperId(this.paperId).
+                fontStyle(this.fontStyle).
+                fontColor(this.fontColor).
+                cardWriter(this.cardWriter)
+                .build();
+    }
+
+}
