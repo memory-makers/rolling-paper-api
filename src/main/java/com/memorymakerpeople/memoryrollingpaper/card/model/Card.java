ddd1c6b197f262fe677a70ed4f9621c8ffc0ee38
mntdev <mntdev@mbc.co.kr>
2022-10-17 11:03:25 +0900

    #12 refactor: 엔티티 리펙토링

--- src/main/java/com/memorymakerpeople/memoryrollingpaper/card/model/Card.java
+++ src/main/java/com/memorymakerpeople/memoryrollingpaper/card/model/Card.java
@@ -5,9 +5,10 @@ import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // new 클래스() 제한
+@ToString(callSuper = true)
+@EqualsAndHashCode
 @Getter
-@Setter
 @Entity
 @DynamicInsert // null로 들어가야 하는 경우 대응(기본값)
 @DynamicUpdate // 변경한 필드만 대응
@@ -16,11 +17,22 @@ public class Card {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int cardId;
+    private Long cardId;
     private String cardText;
     private String cardColor;
-    private String paperId;
+    private Long paperId;
     private String fontStyle;
     private String fontColor;
     private String cardWriter;
+
+    @Builder
+    public Card(Long cardId, String cardText, String cardColor, Long paperId, String fontStyle, String fontColor, String cardWriter) {
+        this.cardId = cardId;
+        this.cardText = cardText;
+        this.cardColor = cardColor;
+        this.paperId = paperId;
+        this.fontStyle = fontStyle;
+        this.fontColor = fontColor;
+        this.cardWriter = cardWriter;
+    }
 }
--- src/main/java/com/memorymakerpeople/memoryrollingpaper/paper/model/Paper.java
+++ src/main/java/com/memorymakerpeople/memoryrollingpaper/paper/model/Paper.java
@@ -1,20 +1,20 @@
 package com.memorymakerpeople.memoryrollingpaper.paper.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
+import java.sql.Time;
 import java.sql.Timestamp;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // new 클래스() 제한
+@ToString(callSuper = true)
+@EqualsAndHashCode
 @Getter
-@Setter
 @Entity
-@DynamicUpdate // 변경한 필드만 대응
 @DynamicInsert // null로 들어가야 하는 경우 대응(기본값)
+@DynamicUpdate // 변경한 필드만 대응
 @Table(name = "tbl_paper")
 public class Paper {
 
@@ -25,8 +25,18 @@ public class Paper {
     private String theme;
     private String paperUrl;
     private String deleteYn;
-    private String userId;
-    private Timestamp createdAt;
-    private Timestamp updatedAt;
+    private Long userId;
     private Timestamp dueDate;
+
+
+    @Builder
+    public Paper(Long paperId, String paperTitle, String theme, String paperUrl, String deleteYn, Long userId, Timestamp dueDate) {
+        this.paperId = paperId;
+        this.paperTitle = paperTitle;
+        this.theme = theme;
+        this.paperUrl = paperUrl;
+        this.deleteYn = deleteYn;
+        this.userId = userId;
+        this.dueDate = dueDate;
+    }
 }
